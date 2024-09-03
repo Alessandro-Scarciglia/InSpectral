Fixed scaling of K according to resolution. Training coded and functioning. Not verified as for performance, but makes sense (parameters twiking and batching is necessary).

embedder.py
@@ -132,7 +132,7 @@ class HashEmbedder(nn.Module):
         # Iterate the forwarding for each resolution
         for i in range(self.n_levels):
             resolution = torch.floor(self.low_res * (self.b ** i))
-
+            
             # Get voxels from coordinates
             voxel_min, voxel_max, hashed_idx, keep_mask = self.get_voxel_vertices(coords,
                                                                                   self.bbox,
@@ -153,7 +153,7 @@ class HashEmbedder(nn.Module):
             keep_mask = keep_mask.sum(dim=-1) == keep_mask.shape[-1]
             new_coords = torch.cat(coords_embedded_all, dim=-1)
 
-            return new_coords, keep_mask
+        return new_coords, keep_mask
 
 
 

parameters.py
@@ -7,33 +7,32 @@ class RenderingParameters:
 
         # Parameters
         self.parameters = {
-            "H": 256,
-            "W": 256,
+            "H": 32,
+            "W": 32,
             "CH": 3,
-            "K": torch.rand(3, 3),
 
             "n_ray_samples": 64,
-            "near": 1.,
-            "far": 7.,
+            "near": 2.,
+            "far": 6.,
 
             "input_dim": 3,
             "degree": 4,
             "out_dim": 16,
 
-            "bbox": (torch.tensor([0., 0., 0.]),
-                     torch.tensor([10., 10., 10.])),
+            "bbox": (torch.tensor([-5., -5., -5.]),
+                     torch.tensor([5., 5., 5.])),
             "n_levels": 16,
-            "n_features_per_level": 10,
+            "n_features_per_level": 2,
             "log2_hashmap_size": 19,
-            "low_res": 64,
+            "low_res": 16,
             "high_res": 512,
 
-            "n_layers": 3,
+            "n_layers": 2,
             "hidden_dim": 64,
             "geo_feat_dim": 15,
             "n_layers_color": 3,
             "hidden_dim_color": 64,
-            "input_ch": 10,          # It shall be equal to "n_features_per_level"
+            "input_ch": 32,          # It shall be equal to "n_features_per_level" * "n_levels"
             "input_ch_views": 16,    # It shall be equal to "out_dim"
             "out_ch": 3,
         }
@@ -56,14 +55,14 @@ class TrainingParameters:
 
         # Parameters
         self.parameters = {
-            "lr": 1e-3,
+            "lr": 0.01,
             "betas": (0.9, 0.999),
             "eps": 1e-8,
             "weight_decay": 0,
             "degenerated_to_sgd": False,
             "tot_var_weight": 1e-6,
             "sparsity_loss_weight": 1e-6,
-            "decay_rate": 1e-1,
+            "decay_rate": 10,
             "decay_steps": 1000
             }
 

rays_generator.py
@@ -2,6 +2,8 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import json
+import matplotlib.pyplot as plt
 
 
 class RaysGenerator(nn.Module):
@@ -42,7 +44,7 @@ class RaysGenerator(nn.Module):
         
         # Rotate directions according to c2w
         rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], dim=-1)
-
+        
         # Translate camera origin in word frame
         rays_o = c2w[:3, -1].expand(rays_d.shape)
 
@@ -50,8 +52,7 @@ class RaysGenerator(nn.Module):
     
 
     def forward(self,
-                c2w: torch.Tensor,
-                frame: torch.Tensor = torch.tensor([])):
+                c2w: torch.Tensor):
         '''
         The forward function returns both true color channels and rays when in "train" mode.
         Viceversa, it returns only rays when in inference-only mode.
@@ -59,31 +60,32 @@ class RaysGenerator(nn.Module):
 
         # Compute rays 
         rays_o, rays_d = self.get_rays(c2w)
-
-        # If the the mode is 'training', returns rays (input) and color channels (labels)
-        if frame.shape[0] != 0:
-            out = torch.concatenate([rays_o, rays_d, frame], dim=-1).reshape(-1, 6 + self.CH)
-
-        # Otherwise, return rays only (input)
-        else:
-            out = torch.concatenate([rays_o, rays_d], dim=-1).reshape(-1, 6)
-
+        
+        # Return rays origins and directions
+        out = torch.concatenate([rays_o, rays_d], dim=-1).reshape(-1, 6)
+    
         return out
-
 
 
 # Run for usage example
 if __name__ == "__main__":
 
-    # Generate random inputs
-    H, W, CH = 64, 64, 3
-    K = torch.rand((3, 3))
-    c2w = torch.rand((4, 4))
-    frame = torch.rand((H, W, CH))
+    # Generate inputs
+    H, W, CH = 32, 32, 3
+    
+    # Load calibration
+    with open("calibration/calibration.json", "r") as fopen:
+        K = torch.tensor(json.load(fopen)["mtx"]).reshape(3, 3)
 
+    # Generaterandom c2w and add a translation
+    c2w = torch.eye(4)
+    c2w[:3, -1] = torch.tensor([0., 0., 10.])
+    
     # Istantiate the model object
     prep = RaysGenerator(H=H, W=W, CH=CH, K=K)
-    output = prep(c2w, frame)
+
+    # Generate rays
+    output = prep(c2w)
 
     # Show
-    print(output)
+    print(output.shape)

rendering.py
@@ -1,6 +1,8 @@
 # Import modules
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Import custom modules
 from parameters import RenderingParameters
@@ -81,11 +83,9 @@ class NeuralRenderer(nn.Module):
         # Hash-encode the samples coordinates and SH-encode the view directions
         enc_points, keep_mask = self.embedder(rays_and_viewdirs[..., :3])
         enc_dirs = self.sh_encoder(rays_and_viewdirs[..., 3:6])
-
-        # Concatenate as a whole input vector and compute a forward pass with NeRF
+        
+        # Concatenate as a whole input vector and compute a forward pass with SmallNeRF
         input_vector = torch.cat([enc_points, enc_dirs], dim=-1)
-        print(enc_dirs.shape)
-
         output = self.nerf(input_vector)
 
         # Clean (i.e. set sigma to 0) output estimates out of bbox boundaries
@@ -95,7 +95,7 @@ class NeuralRenderer(nn.Module):
         
         # Integrate densities and channels values estimate along each ray
         chs_map, depth_map, sparsity_loss = self.integrator(output, zvals, rays[..., 3:6])
-
+        
         # Resize frames
         chs_map = chs_map.reshape((self.rays_generator.H,
                                   self.rays_generator.W,
@@ -103,9 +103,8 @@ class NeuralRenderer(nn.Module):
         
         depth_map = depth_map.reshape((self.rays_generator.H,
                                       self.rays_generator.W))
-       
+
         return chs_map, depth_map, sparsity_loss
-         
 
 
 # Run for usage example
@@ -115,20 +114,19 @@ if __name__ == "__main__":
     params_obj = RenderingParameters()
     params = params_obj.get_all_params()
 
-    # Instantiate the model object
-    renderer = NeuralRenderer(**params)
+    # Retrieve intrinsic calibration matrix K
+    K = np.array([[731.2, 0.0, 512.0], [0.0, 731.2, 512], [0.0, 0.0, 1.0]])
 
-    # Generate dummy inputs
-    dummy_c2w = torch.rand((4, 4), dtype=torch.float32)
+    # Instantiate the model object
+    renderer = NeuralRenderer(**params, K=K)
+
+    # Generate c2w and add a translation
+    c2w = torch.eye(4)
+    c2w[:3, -1] = torch.tensor([0., 0., 5.])
 
     # Infer
-    frame, depth, sparsity = renderer(dummy_c2w)
-    
-    # Parameters
-    print(len(list(renderer.rays_generator.parameters())))
-    print(len(list(renderer.sampler.parameters())))
-    print(len(list(renderer.embedder.parameters())))
-    print(len(list(renderer.nerf.parameters())))
-    print(len(list(renderer.integrator.parameters())))
-    print("---")
-    print(len(list(renderer.parameters())))
+    frame, depth, sparsity = renderer(c2w)
+
+    # Test
+    plt.hist(frame.detach().numpy().reshape(-1), bins=30)
+    plt.show()

sampler.py
@@ -22,7 +22,7 @@ class Sampler(nn.Module):
         '''
         This function generate n_samples on a batch of rays which start from point
         rays_o (origins) and propagate toward rays_d (directions), within the
-        rendering region [near, far] and the rendering bbox [bbox_min, bbox_max].
+        rendering region [near, far].
         '''
 
         # Useful variables
@@ -63,4 +63,4 @@ if __name__ == "__main__":
     samples = sampler(rays_o, rays_d)
 
     # No params
-    print(list(sampler.parameters()))
+    print(samples)

sat_node.py
@@ -2,6 +2,8 @@
 import json
 import torch
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Custom modules
 from virtual_sensors import VirtualSensors
@@ -24,7 +26,8 @@ class SatNode:
         # Retrieve intrinsic calibration matrix K
         with open(calibration_path, "r") as fopen:
             calib = json.load(fopen)
-            self.k = calib["mtx"]
+            self.k = np.array(calib["mtx"]).reshape(3, 3)
+            self.k[:2, :3] /= (1024/resolution)
 
         # Initialize measurements generator
         vsens = VirtualSensors(datapath=datapath,
@@ -39,7 +42,7 @@ class SatNode:
 
         # Create NeRF Renderer
         renderer_args = RenderingParameters()
-        self.renderer = NeuralRenderer(**renderer_args.get_all_params())
+        self.renderer = NeuralRenderer(**renderer_args.get_all_params(), K=self.k)
 
         # Create Training routine
         training_args = TrainingParameters()
@@ -58,24 +61,19 @@ class SatNode:
 # Run for test
 if __name__ == "__main__":
     sat1 = SatNode(roll_cfg="roll_120",
-                   resolution=64,
+                   resolution=32,
                    datapath='data/transforms.json',
                    calibration_path='calibration/calibration.json')
 
-    for i, (c2w, frame) in enumerate(sat1.get_measurement()):
-        
-        # Convert to tensors
-        c2w = torch.tensor(c2w, dtype=torch.float32)
-        frame = torch.tensor(frame, dtype=torch.float32)
+    for i, (c2w, frame) in enumerate(sat1.get_measurement()): 
         
         # Show every 10 samples
         if i % 10 == 0:
             with torch.no_grad():
                 test_chs_map, _, __, = sat1.render(c2w)
-                cv2.imshow("", test_chs_map.detach().numpy())
+                cv2.imshow("", test_chs_map.detach().numpy() * 255.)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
 
-        
         loss = sat1.trainer.train_one_frame(c2w=c2w, frame=frame, niter=i)
         print(loss.item())

small_nerf.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 
 class NeRFSmall(nn.Module):
@@ -105,12 +106,12 @@ class NeRFSmall(nn.Module):
         '''
         Inference method.
         '''
-
+        
         # Split origin
         input_pts, input_views = torch.split(cam_rays,
                                              [self.input_ch, self.input_ch_views],
                                              dim=-1)
-        
+
         # Sigma estimation branch
         out = input_pts
         for layer in range(self.n_layers):
@@ -135,7 +136,7 @@ class NeRFSmall(nn.Module):
         # Extract color and produce inference output
         color = out
         outputs = torch.cat([color, sigma.unsqueeze(-1)], dim=-1)
-
+        
         return outputs
     
 
@@ -152,5 +153,6 @@ if __name__ == "__main__":
     # Try inference and test input/output dimension
     out = model(dummy_input)
     print(dummy_input.shape, out.shape)
-    print(len(list(model.parameters())))
+    plt.hist(out[:, -1].detach().numpy().reshape(-1)*255, bins=100)
+    plt.show()
     

virtual_sensors.py
@@ -1,7 +1,7 @@
 # Modules import
 import cv2
 import json
-import numpy as np
+import torch
 
 
 class VirtualSensors:
@@ -50,7 +50,7 @@ class VirtualSensors:
 
             # TODO: add optional drift
 
-            yield np.array(c2w), resized_frame
+            yield torch.tensor(c2w), torch.tensor(resized_frame) / 255.0
 
 
 # Run for usage example
@@ -74,6 +74,7 @@ if __name__ == "__main__":
         
         # Visualize frame
         print(frame.shape)
-        cv2.imshow("", frame)
+        cv2.imshow("", frame.numpy())
         cv2.waitKey(0)
+        exit()
     